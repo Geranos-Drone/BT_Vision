ca9d40cf01c25804d767e0381321ffc1ea5ae01d
TimReX-22 <tim.reinhart@bluewin.ch>
2022-05-24 13:10:00 +0200

    added test prediction file

---
+++ predictions_test.py
@@ -0,0 +1,48 @@
+import io
+import numpy as np
+import PIL
+from PIL import Image
+import torchvision
+import requests
+import torch
+import openpifpaf
+
+# Idea: make predictor preprocessing identical to train preprocessing!
+
+
+#import constants
+from openpifpaf_pole_plugin.constants import POLE_KEYPOINTS, POLE_SKELETON, POLE_SIGMAS, POLE_CATEGORIES, POLE_POSE, POLE_SCORE_WEIGHTS
+from openpifpaf import encoder, headmeta, metric, transforms
+
+
+print('OpenPifPaf version', openpifpaf.__version__)
+print('PyTorch version', torch.__version__)
+
+cif = headmeta.Cif('cif', 'pole_detect',
+                   keypoints=POLE_KEYPOINTS,
+                   sigmas=POLE_SIGMAS,
+                   pose=POLE_POSE,
+                   draw_skeleton=POLE_SKELETON,
+                   score_weights=POLE_SCORE_WEIGHTS)
+
+caf = headmeta.Caf('caf', 'pole_detect',
+                   keypoints=POLE_KEYPOINTS,
+                   sigmas=POLE_SIGMAS,
+                   pose=POLE_POSE,
+                   skeleton=POLE_SKELETON)
+
+head_metas = [cif, caf]
+
+pil_im = Image.open("/home/tim/BT_Vision/convert_to_coco/test_dataset_coco/images/train/220511_1001.jpg")
+im = np.asarray(pil_im)
+
+predictor = openpifpaf.Predictor(checkpoint='outputs/mobilenetv2-220523-163750-pole_detect.pkl.epoch170', head_metas=head_metas) 
+predictions, gt_anns, image_meta = predictor.numpy_image(im)
+
+if len(predictions) == 0:
+    print("No Keypoints found!")
+
+else:
+    print(predictions[0].data)
+
+input("Press Enter to stop...")
