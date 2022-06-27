67465bc0a5ca1b98e73dc811287c26349db638bd
Nicolas <gorlon@student.ethz.ch>
2022-06-27 14:51:08 +0200

    adapted train_test_split to diverse dataset

--- convert_to_coco/train_test_split.py
+++ convert_to_coco/train_test_split.py
@@ -2,9 +2,9 @@ import os
 import random
 import json
 
-img_dir = "vicon_dataset/images"
-ls_path = "vicon_dataset/annotations/ls_export.json"
-ls_path_old = "vicon_dataset/annotations/ls_export_old.json"
+img_dir = "vicon_dataset_div/images"
+ls_path = "vicon_dataset_div/annotations/ls_export.json"
+ls_path_old = "vicon_dataset_div/annotations/ls_export_old.json"
 
 with open(ls_path_old) as json_file:
     data = json.load(json_file)
@@ -17,12 +17,12 @@ for ann in data:
         data.remove(ann)
 
 images = os.listdir(img_dir)
-print(len(images))
+print("total images: ", len(images))
 
 images_labeled = [x for x in images if x not in skipped_imgs]
 
 
-print(len(images_labeled))
+print("labeled images: ", len(images_labeled))
 
 random.seed(42)
 random.shuffle(images_labeled)
@@ -32,16 +32,16 @@ split = int(len(images_labeled)*0.8)
 train_list = images_labeled[:split]
 val_list = images_labeled[split:]
 
-print(len(train_list))
-print(len(val_list))
+print("train_size: ", len(train_list))
+print("val_size: ", len(val_list))
 
 
-train_txt = open("vicon_dataset/split/train.txt", "w")
+train_txt = open("vicon_dataset_div/split/train.txt", "w")
 for element in train_list:
     train_txt.write(element + "\n")
 train_txt.close()
 
-val_txt = open("vicon_dataset/split/val.txt","w")
+val_txt = open("vicon_dataset_div/split/val.txt","w")
 for element in val_list:
     val_txt.write(element + "\n")
 val_txt.close()
